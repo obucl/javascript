8c94f3a43b456d89f17461f47a4220190022d786
Gozie <55288978+obucl@users.noreply.github.com>
2020-03-23 19:03:09 +0100

    Javascript task 1

---
+++ script.js
@@ -0,0 +1,14 @@
+const name = "Obu Chigozie";
+const courses = ["HTML", "CSS", "JavaScript", "NodeJS"];
+
+console.log('My name is ' + name + ' & My courses are ' + courses);
+
+const evenCourses = []
+
+for (let i=0; i<=200; i++) {
+  if (i%2===0) {
+    evenCourses.push(i);
+  }
+}
+
+console.log(evenCourses);
